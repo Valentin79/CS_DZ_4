60aa119eb0061f9b43358ce4d9837ac790566f73
Valentin <zvezdochet.79@mail.ru>
2023-10-28 14:01:40 +0300

    dz_4

--- DZ.cs
+++ DZ.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,28 +11,30 @@ namespace CS_DZ_Sem_4
     {
         public static List<int> FindThreeNumbers(int[] arr, int target, int startIndex)
         {
-            
+            Array.Sort(arr);
+            arr.ToList().ForEach(Console.Write);
+            Console.WriteLine();
 
             List<int> result = new List<int>();
 
             for (int i = startIndex; i < arr.Length - 2; i++)
             {
-                int left = i + 1;
-                int right = arr.Length - 1;
+                int left = i + 1; // берем число из левой части массива, но не первое, к первому будем прибавлять
+                int right = arr.Length - 1; // берем число из конца массива
 
-                while (left < right)
+                while (left < right)  // пока не дойдем до середины
                 {
-                    int sum = arr[i] + arr[left] + arr[right];
+                    int sum = arr[i] + arr[left] + arr[right]; // суммируем первое число массива с двумя другими по индексам left и right
 
-                    if (sum == target)
+                    if (sum == target) // если результат сошелся, записываем числа в массив
                     {
                         result.Add(arr[i]);
                         result.Add(arr[left]);
                         result.Add(arr[right]);
                         break;
                     }
-                    else if (sum < target) { left++; }
-                    else { right--; }
+                    else if (sum < target) { left++; } // если сумма элементов меньше, в переменную left берем следующее число
+                    else { right--; } // если больше - в переменную right берем предшествующее число
                 }
 
                 if (result.Count > 0) { break; }
