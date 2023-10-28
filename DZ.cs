a713d0fca71e4538ec8af5c5b9eb61a04abc0ae5
Valentin <zvezdochet.79@mail.ru>
2023-10-28 13:44:40 +0300

    dz_4

---
+++ DZ.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_DZ_Sem_4
+{
+    internal class DZ
+    {
+        public static List<int> FindThreeNumbers(int[] arr, int target, int startIndex)
+        {
+            
+
+            List<int> result = new List<int>();
+
+            for (int i = startIndex; i < arr.Length - 2; i++)
+            {
+                int left = i + 1;
+                int right = arr.Length - 1;
+
+                while (left < right)
+                {
+                    int sum = arr[i] + arr[left] + arr[right];
+
+                    if (sum == target)
+                    {
+                        result.Add(arr[i]);
+                        result.Add(arr[left]);
+                        result.Add(arr[right]);
+                        break;
+                    }
+                    else if (sum < target) { left++; }
+                    else { right--; }
+                }
+
+                if (result.Count > 0) { break; }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace CS_DZ_Sem_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int target = 13;
+            int startIndex = 0;
+            int resultCount = 0;
+            while (startIndex < arr.Length)
+            {
+                List<int> result = DZ.FindThreeNumbers(arr, target, startIndex);
+                if (result.Count > 0)
+                {
+                    Console.WriteLine($"{result[0]} + {result[1]} + {result[2]} = {target}");
+                    startIndex++;
+                    resultCount++;
+                }
+                else { startIndex++; }
+            }
+            if (resultCount == 0) { Console.WriteLine("Результатов не найдено"); }
+        }
+    }
+}
